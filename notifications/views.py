114fabcdc5560bf36abbc24808c56325174fbe49
root <root@ip-172-31-0-176.ap-south-1.compute.internal>
2021-04-18 15:33:38 +0000

    Added Max ratings table and updated email template

--- notifications/views.py
+++ notifications/views.py
@@ -9,7 +9,7 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
 from django.core.mail import send_mail
-
+import time
 check_status = False
 
 
@@ -32,51 +32,83 @@ def previous_week_range(date):
     return start_date, end_date
 
 def buildWeeklyUpdate():
+	codeforces_object = dict()
 	start_date,end_date = previous_week_range(date.today())
 	start_date = datetime.combine(start_date,datetime.min.time())
 	end_date = datetime.combine	(end_date,datetime.max.time())
 	user_data = User.objects.all()
+	print(len(user_data))
+	request_count=0	
 	for user in user_data.iterator():
 		req = Request('https://codeforces.com/api/user.rating?handle='+user.codeforces_handle)
+		request_count+=1 
+		if(request_count==5):
+			time.sleep(2)
+			request_count = 0
 		req.add_header('accept-language','en-US')
 		try:
 			resp = urlopen(req)
 			print('I am there')
-			print(resp)
+			# print(resp)
 		except urllib.error.HTTPError as err:
+			print(req)
+			print(resp)
 			print('Error for '+user.codeforces_handle)
 			continue
-	content = json.loads(resp.read().decode('utf-8'))
-	if content['status']!='OK':
-		print('Error for '+user.codeforces_handle)
-		return
-	print(content)
-	minr,maxr=None,None
-	totalMax = 0
-	for contest in content['result']:
-		if start_date<= datetime.fromtimestamp(int(contest['ratingUpdateTimeSeconds'])) and datetime.fromtimestamp(int(contest['ratingUpdateTimeSeconds']))  <= end_date:
-			if minr==None:
-				minr = contest['oldRating']
-				maxr= contest['newRating']
-			else:
-				maxr= contest['newRating']
-			totalMax = max(totalMax,contest['newRating'])
-	print(maxr)
-	if maxr==None:
-		return
-	print('I am at week')
+		content = json.loads(resp.read().decode('utf-8'))
+		if content['status']!='OK':
+			print('Error for status at'+user.codeforces_handle)
+			continue
+		# print('content is '+str(content))
+		minr,maxr=None,None
+		totalMax = 0
+		for contest in content['result']:
+			if start_date<= datetime.fromtimestamp(int(contest['ratingUpdateTimeSeconds'])) and datetime.fromtimestamp(int(contest['ratingUpdateTimeSeconds']))  <= end_date:
+				if minr==None:
+					minr = contest['oldRating']
+					maxr= contest['newRating']
+				else:
+					maxr= contest['newRating']
+				totalMax = max(totalMax,contest['newRating'])
+		print(maxr)
+		if maxr==None:
+			continue
+		print('I am at week')
+		codeforces_object[user.codeforces_handle]=dict()
+		codeforces_object[user.codeforces_handle]['startRating']=minr
+		codeforces_object[user.codeforces_handle]['endRating']=maxr
+		codeforces_object[user.codeforces_handle]['totalMax']= totalMax
+		codeforces_object[user.codeforces_handle]['ratingChange']=maxr-minr
+	if(len(codeforces_object.keys())!=0):
+		start_date_string = str(start_date.date())
+		end_date_string = str(end_date.date())
+		week = WeeklyChange(startdate=start_date_string,enddate=end_date_string,codeforces=codeforces_object)
+		week.save()
+		return (start_date_string, end_date_string)
 
-	start_date = str(start_date.date())
-	end_date = str(end_date.date())
-	codeforces_handle = dict()
-	codeforces_handle['startRating']=minr
-	codeforces_handle['endRating']=maxr
-	codeforces_handle['totalMax']= totalMax
-	codeforces_handle['ratingChange']=maxr-minr
-	week = WeeklyChange(startdate=start_date,enddate=end_date,codeforces=codeforces_handle)
-	week.save()
-
-
+def buildOverallMaxRatings():
+	pass
+	# user_data = User.objects.all()
+	# for user in user_data.iterator():
+	# 	req = Request('https://codeforces.com/api/user.rating?handle='+user.codeforces_handle)
+	# 	request_count+=1 
+	# 	if(request_count==5):
+	# 		time.sleep(2)
+	# 		request_count = 0
+	# 	req.add_header('accept-language','en-US')
+	# 	try:
+	# 		resp = urlopen(req)
+	# 	except urllib.error.HTTPError as err:
+	# 		print('Error for '+user.codeforces_handle)
+	# 		continue
+	# 	content = json.loads(resp.read().decode('utf-8'))
+	# 	if content['status']!='OK':
+	# 		print('Error for status at'+user.codeforces_handle)
+	# 		continue
+	# 	tempmax = 
+	# 	for contest in content['result']:
+	# 		totalMax = max(totalMax,contest['newRating'])
+	# 	print(maxr)
 
 def start_view(request):
 	u=User()
@@ -93,6 +125,7 @@ def run_view(request):
         return HttpResponse('Automation request is already in progress')
     check_status = True
     buildWeeklyUpdate()
+    buildOverallMaxRatings()
     return HttpResponse('Request returned')
 
 
